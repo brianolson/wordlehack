745007b4bc8366d372b13d68f0b32d214581986b
Brian Olson <brian.olson@gmail.com>
2022-01-20 06:22:36 -0500

    cleanup

--- wordlehack.go
+++ wordlehack.go
@@ -10,28 +10,6 @@ import (
 	"sync"
 )
 
-/*
-posc = []
-for _,guess in scores:
-    count = 0
-    for target in la:
-        if guess == target:
-            continue
-        res = matchres(target,guess)
-        for w in la:
-            hit = True
-            for rei in res:
-                m = rei.match(w)
-                if not m:
-                    hit = False
-                    break
-            if hit:
-                count += 1
-    print(guess,count)
-    posc.append((count,guess))
-posc.sort()
-*/
-
 func maybefail(err error, msg string, args ...interface{}) {
 	if err == nil {
 		return
@@ -100,7 +78,6 @@ func guessThread(la []string, guesses chan string, out chan StringInt, wg *sync.
 			}
 		}
 		out <- StringInt{guess, count}
-		//fmt.Printf("%s\t%d\n", guess, count)
 	}
 }
 
@@ -118,9 +95,6 @@ func main() {
 	var la []string
 	err = dec.Decode(&la)
 	maybefail(err, "La.json []string: %v", err)
-	// var hasb [5]byte
-	// var notb [5]byte
-	// var posb [5]string
 	wg := sync.WaitGroup{}
 	guesses := make(chan string, 20)
 	results := make(chan StringInt, 20)
@@ -129,56 +103,6 @@ func main() {
 		wg.Add(1)
 	}
 	go submitter(la, guesses)
-	/*
-		gb := []byte(guess)
-		count := 0
-		for ti, target := range la {
-			if ti == gi {
-				continue
-			}
-			has := hasb[:0]
-			not := notb[:0]
-			for gci, gc := range gb {
-				if gc == target[gci] {
-					posb[gci] = string(gb[gci : gci+1])
-				} else if strings.IndexByte(target, gc) != -1 {
-					has = append(has, gc)
-					posb[gci] = fmt.Sprintf("[^%c]", gc)
-				} else {
-					posb[gci] = "."
-					not = append(not, gc)
-				}
-			}
-			res := strings.Join(posb[:], "")
-			re := regexp.MustCompile(res)
-
-			for _, w := range la {
-				if !re.MatchString(w) {
-					continue
-				}
-				hit := true
-				for _, c := range has {
-					if strings.IndexByte(w, c) == -1 {
-						hit = false
-						break
-					}
-				}
-				if !hit {
-					continue
-				}
-				for _, c := range not {
-					if strings.IndexByte(w, c) != -1 {
-						hit = false
-						break
-					}
-				}
-				if !hit {
-					continue
-				}
-				count++
-			}
-		}
-	*/
 	for xr := range results {
 		guess := xr.S
 		count := xr.I
