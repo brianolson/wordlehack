b790287178566fa329131540de3814e589cc8001
Brian Olson <brian.olson@gmail.com>
2022-01-19 20:08:34 -0500

    multicore

--- wordlehack.go
+++ wordlehack.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"runtime"
 	"strings"
+	"sync"
 )
 
 /*
@@ -38,6 +40,77 @@ func maybefail(err error, msg string, args ...interface{}) {
 	os.Exit(1)
 }
 
+type StringInt struct {
+	S string
+	I int
+}
+
+func guessThread(la []string, guesses chan string, out chan StringInt, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var hasb [5]byte
+	var notb [5]byte
+	var posb [5]string
+	for guess := range guesses {
+		gb := []byte(guess)
+		count := 0
+		for _, target := range la {
+			if target == guess {
+				continue
+			}
+			has := hasb[:0]
+			not := notb[:0]
+			for gci, gc := range gb {
+				if gc == target[gci] {
+					posb[gci] = string(gb[gci : gci+1])
+				} else if strings.IndexByte(target, gc) != -1 {
+					has = append(has, gc)
+					posb[gci] = fmt.Sprintf("[^%c]", gc)
+				} else {
+					posb[gci] = "."
+					not = append(not, gc)
+				}
+			}
+			res := strings.Join(posb[:], "")
+			re := regexp.MustCompile(res)
+
+			for _, w := range la {
+				if !re.MatchString(w) {
+					continue
+				}
+				hit := true
+				for _, c := range has {
+					if strings.IndexByte(w, c) == -1 {
+						hit = false
+						break
+					}
+				}
+				if !hit {
+					continue
+				}
+				for _, c := range not {
+					if strings.IndexByte(w, c) != -1 {
+						hit = false
+						break
+					}
+				}
+				if !hit {
+					continue
+				}
+				count++
+			}
+		}
+		out <- StringInt{guess, count}
+		//fmt.Printf("%s\t%d\n", guess, count)
+	}
+}
+
+func submitter(la []string, guesses chan string) {
+	for _, guess := range la {
+		guesses <- guess
+	}
+	close(guesses)
+}
+
 func main() {
 	fin, err := os.Open("La.json")
 	maybefail(err, "La.json: %v", err)
@@ -45,10 +118,18 @@ func main() {
 	var la []string
 	err = dec.Decode(&la)
 	maybefail(err, "La.json []string: %v", err)
-	var hasb [5]byte
-	var notb [5]byte
-	var posb [5]string
-	for gi, guess := range la {
+	// var hasb [5]byte
+	// var notb [5]byte
+	// var posb [5]string
+	wg := sync.WaitGroup{}
+	guesses := make(chan string, 20)
+	results := make(chan StringInt, 20)
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go guessThread(la, guesses, results, &wg)
+		wg.Add(1)
+	}
+	go submitter(la, guesses)
+	/*
 		gb := []byte(guess)
 		count := 0
 		for ti, target := range la {
@@ -97,6 +178,10 @@ func main() {
 				count++
 			}
 		}
+	*/
+	for xr := range results {
+		guess := xr.S
+		count := xr.I
 		fmt.Printf("%s\t%d\n", guess, count)
 	}
 }
